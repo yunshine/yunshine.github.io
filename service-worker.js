e4d45a66fc00de31c0457a1ca13959c64c2527c7
Yun Chung <yun.chung@mac.com>
2020-09-17 12:23:17 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.58d48f3c8dffbd321a4289c64e359a17.js"
+  "/precache-manifest.5a5dfbbe3f999e39e7e353bfa44f9eb0.js"
 );
 
 self.addEventListener('message', (event) => {
