daf6f7753ef270ee2556943e04b7594c1c510a31
Yun Chung <yun.chung@mac.com>
2020-12-01 12:15:23 +0900

    Fixed CV error and deployed

--- build/precache-manifest.1c6412b91528951c59a9538f5ff78d04.js
+++ build/precache-manifest.1c6412b91528951c59a9538f5ff78d04.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bcfda6f53c28be4cc967724a24ccea58",
+    "revision": "da241fbeda28f4aeed2dfc7773aeaf47",
     "url": "/index.html"
   },
   {
-    "revision": "6a2eedbd57383c313f02",
+    "revision": "63caf0f1a34251e8a62d",
     "url": "/static/css/main.88e99f5a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c52af980.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a2eedbd57383c313f02",
-    "url": "/static/js/main.909ebe84.chunk.js"
+    "revision": "63caf0f1a34251e8a62d",
+    "url": "/static/js/main.9a285087.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
@@ -60,8 +60,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/OneBite3.3c157bd9.jpg"
   },
   {
-    "revision": "55ac91e4e37facb0ab486b56fc185471",
-    "url": "/static/media/YunChungCV.55ac91e4.pdf"
+    "revision": "00f7b12053568f0ad2aa85c5260ae7a2",
+    "url": "/static/media/YunChungCV.00f7b120.pdf"
   },
   {
     "revision": "2f43fb6a19c08b2b12b8b7d5c792b21c",
