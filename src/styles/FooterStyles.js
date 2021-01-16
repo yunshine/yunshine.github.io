90334ac5855c4265d741460ff8532ed0ab975baf
Yun Chung <yun.chung@mac.com>
2021-01-16 13:22:51 +0900

    Footer styles update

--- src/styles/FooterStyles.js
+++ src/styles/FooterStyles.js
@@ -1,39 +1,41 @@
 import sizes from './MediaSizes';
 
 export default {
-  Footer: {
-    background: 'rgba(53,161,205,0.85)',
-    marginBottom: '0px',
-    padding: '16px',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    '&  h5': {
-      marginBottom: '0px',
-      color: ' rgba(254, 254, 254, 1)',
-      fontWeight: '200',
-      fontSize: '18px',
+    Footer: {
+        background: 'rgba(53,161,205,0.85)',
+        marginBottom: '0px',
+        padding: '16px 32px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        '&  h5': {
+            margin: '0px',
+            padding: '0px',
+            color: ' rgba(254, 254, 254, 1)',
+            fontWeight: '200',
+            fontSize: '18px',
+        },
+        [sizes.down('md')]: {
+            fontSize: '16px',
+            padding: '16px 16px',
+        },
+        [sizes.down('sm')]: {
+            fontSize: '12px',
+        },
     },
-    [sizes.down('md')]: {
-      fontSize: '16px',
+    iconDiv: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        '& i': {
+            fontSize: '24px',
+            color: ' rgba(254, 254, 254, 0.85)',
+            fontWeight: '200',
+            margin: '0px 12px',
+            transition: 'all .1s ease-in-out',
+            '&:hover': {
+                transform: 'scale(1.3)',
+            },
+        },
     },
-    [sizes.down('sm')]: {
-      fontSize: '12px',
-    },
-  },
-  iconDiv: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    '& i': {
-      fontSize: '24px',
-      color: ' rgba(254, 254, 254, 0.85)',
-      fontWeight: '200',
-      margin: '0px 12px',
-      transition: 'all .1s ease-in-out',
-      '&:hover': {
-        transform: 'scale(1.3)',
-      },
-    },
-  },
 };
